bdcd0d4e60585b7a24cdcbc04dd4d7fc2f84ff32
Ali-Massrany <alimassrany453099@gmail.com>
2022-07-13 22:03:18 +0300

    redeploy3

--- firebase.js
+++ firebase.js
@@ -1,14 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5q864mdH3qsbSbr-xKWIzHsWvEwZ9RS0",
-  authDomain: "drive-clone-c3c62.firebaseapp.com",
-  projectId: "drive-clone-c3c62",
-  storageBucket: "drive-clone-c3c62.appspot.com",
-  messagingSenderId: "696876222450",
-  appId: "1:696876222450:web:1862ff432e6e8b7026cd6b",
+  apiKey: "AIzaSyACf68t0dB5WN3UNFrpnlHcWFSSf30da7w",
+  authDomain: "docs-fee5a.firebaseapp.com",
+  projectId: "docs-fee5a",
+  storageBucket: "docs-fee5a.appspot.com",
+  messagingSenderId: "432424145278",
+  appId: "1:432424145278:web:e4f6369bcb9b5ad4bea32f",
 };
-
 const app = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
   : firebase.app();
