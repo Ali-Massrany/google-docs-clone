8b2e6e82d444c56d58244141a873800bf4752af3
Ali-Massrany <alimassrany453099@gmail.com>
2022-07-14 18:56:39 +0300

    redeploy

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,16 +1,16 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
-
+  secret: process.env.NEXTAUTh_SECRET,
   // // A database is optional, but required to persist accounts in a database
   // database: process.env.DATABASE_URL,
-})
+});
